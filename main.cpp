09f5a23fd6675098cd23eadb05f0227140b5910d
Alvineornela <127629007+Alvineornela@users.noreply.github.com>
2023-03-29 23:52:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@ int main(){
     float areatriangolo,areaquadrato,arearettangolo,areatrapezio;
   std::cout << "inserire tre numeri reali per calcolare area  \n";
 
-cin >> a >> b >> c;
+std::cin >> a >> b >> c;
 
   areatriangolo=(a*b)/2;
     areaquadrato=(a*a);
     arearettangolo=(b*a);
     areatrapezio=((a+b)*c)/2;
-  st::cout<<"area triangolo = "<<areatriangolo<<std::endl;
+  std::cout<<"area triangolo = "<<areatriangolo<<std::endl;
     std::cout<<"area quadrato = "<<std::areaquadrato<<endl;
     std::cout<<"area rettangolo = "<<arearettangolo<<std::endl;
     std::cout<<"area trapezio = "<<areatrapezio<<std::endl;
