86881f8ce6761824568f8e7e93c15b326076ae71
Alvineornela <127629007+Alvineornela@users.noreply.github.com>
2023-03-29 23:40:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,21 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
+  float a,b,c;
+    float areatriangolo,areaquadrato,arearettangolo,areatrapezio;
+  cout << "inserire tre numeri reali per calcolare area  \n";
 
 cin >> a >> b >> c;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
+  areatriangolo=(a*b)/2;
+    areaquadrato=(a*a);
+    arearettangolo=(b*a);
+    areatrapezio=((a+b)*c)/2;
+  cout<<"area triangolo = "<<areatriangolo<<endl;
+    cout<<"area quadrato = "<<areaquadrato<<endl;
+    cout<<"area rettangolo = "<<arearettangolo<<endl;
+    cout<<"area trapezio = "<<areatrapezio<<endl;
 
+return 0;
 
 }
