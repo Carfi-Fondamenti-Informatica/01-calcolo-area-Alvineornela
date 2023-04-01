60ba1fbb967633439a13467c670e28cd8d6f5923
Alvineornela <127629007+Alvineornela@users.noreply.github.com>
2023-04-01 20:55:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main(){
   float a=0,b=0,c=0;
     float areatriangolo=0,areaquadrato=0,arearettangolo=0,areatrapezio=0;
-  cout << "inserire tre numeri reali \n";
 cin >> a >> b >> c;
    areatriangolo=(a*b)/2;
     areaquadrato=(a*a);
